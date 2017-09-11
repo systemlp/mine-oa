3270e7e5c14808a707e51c5ac28f09e6123b9711
v_liupeng <system_lp@126.com>
2017-09-11 14:37:42 +0800

    二叉排序树实现

---
+++ src/main/java/com/mine/oa/util/OrderBinary.java
@@ -0,0 +1,154 @@
+package com.mine.oa.util;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+/***
+ *
+ * 〈二叉排序树〉<br>
+ * 〈功能详细描述〉
+ *
+ * @author liupeng
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
+public class OrderBinary<T extends Comparable> {
+
+    private class Node {
+        private T key;
+        private Node left;
+        private Node right;
+
+        Node(T key) {
+            this.key = key;
+        }
+    }
+
+    /** 根节点 */
+    private Node rootNode;
+
+    public OrderBinary(List<T> dataList) {
+        this.rootNode = new Node(dataList.get(0));
+        for (int i = 1; i < dataList.size(); i++) {
+            convertToBinary(this.rootNode, dataList.get(i));
+        }
+    }
+
+    /***
+     * 将数据存储在树的节点中，小于当前节点放入左子节点中，反之放在右子节点中
+     * 
+     * @param node 节点
+     * @param data 数据
+     * @return 节点
+     */
+    private Node convertToBinary(Node node, T data) {
+        if (node == null) {
+            node = new Node(data);
+            return node;
+        }
+        if (data.compareTo(node.key) < 0) {
+            node.left = convertToBinary(node.left, data);
+        } else {
+            node.right = convertToBinary(node.right, data);
+        }
+        return node;
+    }
+
+    /***
+     * 对树进行升序排列
+     * 
+     * @return 升序结果
+     */
+    public List<T> ascOrder() {
+        List<Node> ascOrder = ascOrder(this.rootNode);
+        List<T> list = Lists.newArrayList();
+        for (Node node : ascOrder) {
+            list.add(node.key);
+        }
+        return list;
+    }
+
+    /***
+     * 先取出左下的子节点，其次取出当前节点，最后取出右下的子节点
+     * 
+     * @param node 当前节点
+     * @return 升序节点
+     */
+    private List<Node> ascOrder(Node node) {
+        List<Node> nodeList = Lists.newArrayList();
+        if (node.left != null) {
+            nodeList.addAll(ascOrder(node.left));
+        }
+        nodeList.add(node);
+        if (node.right != null) {
+            nodeList.addAll(ascOrder(node.right));
+        }
+        return nodeList;
+    }
+
+    /***
+     * 对树进行降序排列
+     * 
+     * @return 升序结果
+     */
+    public List<T> descOrder() {
+        List<Node> descOrder = descOrder(this.rootNode);
+        List<T> list = Lists.newArrayList();
+        for (Node node : descOrder) {
+            list.add(node.key);
+        }
+        return list;
+    }
+
+    /***
+     * 先取出右下的子节点，其次取出当前节点，最后取出左下的子节点
+     * 
+     * @param node 前节点
+     * @return 降序节点
+     */
+    private List<Node> descOrder(Node node) {
+        List<Node> nodeList = Lists.newArrayList();
+        if (node.right != null) {
+            nodeList.addAll(descOrder(node.right));
+        }
+        nodeList.add(node);
+        if (node.left != null) {
+            nodeList.addAll(descOrder(node.left));
+        }
+        return nodeList;
+    }
+
+    public T min() {
+        Node node = this.rootNode;
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node.key;
+    }
+
+    public T max() {
+        Node node = this.rootNode;
+        while (node.right != null) {
+            node = node.right;
+        }
+        return node.key;
+    }
+
+    public boolean contain(T data){
+        return contain(this.rootNode, data);
+    }
+
+    private boolean contain(Node node, T data) {
+        if (node == null) {
+            return false;
+        }
+        if (node.key.equals(data)) {
+            return true;
+        } else if (node.key.compareTo(data) > 0) {
+            return contain(node.left, data);
+        }
+        return contain(node.right, data);
+    }
+
+}
--- src/test/java/com/mine/oa/service/TestUserService.java
+++ src/test/java/com/mine/oa/service/TestUserService.java
@@ -1,9 +1,17 @@
 package com.mine.oa.service;
 
+import com.google.common.collect.Lists;
 import com.mine.oa.TestBase;
 import com.mine.oa.dto.UserLoginDto;
+import com.mine.oa.util.OrderBinary;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 /***
  *
@@ -25,6 +33,26 @@ public class TestUserService extends TestBase {
         loginDto.setUserName("admin");
         loginDto.setPassword("123456");
         System.out.println(userService.login(loginDto));
+    }
+
+    @Test
+    public void testOrderBinary() {
+        List<Integer> dataList = Lists.newArrayList(3, 2, 7, 7, 9, 6, 100, -5);
+        System.out.println(StringUtils.join(dataList, ","));
+        long cutTime = System.currentTimeMillis();
+        OrderBinary<Integer> orderBinaryU = new OrderBinary<>(dataList);
+        List<Integer> ascData = orderBinaryU.ascOrder();
+        System.out.println(System.currentTimeMillis() - cutTime + " asc->" + StringUtils.join(ascData, ","));
+        List<Integer> descData = orderBinaryU.descOrder();
+        System.out.println("desc->" + StringUtils.join(descData, ","));
+        System.out.println("min->" + orderBinaryU.min());
+        System.out.println("max->" + orderBinaryU.max());
+        int data = 5;
+        System.out.println(data + (orderBinaryU.contain(data) ? " is found!" : " is not found!"));
+        // dataList = Lists.newArrayList(3, 2, 7, 9, 6, 100, -5);
+        cutTime = System.currentTimeMillis();
+        dataList.sort(null);
+        System.out.println(System.currentTimeMillis() - cutTime/* + "->" + StringUtils.join(ascData, ",") */);
     }
 
 }
